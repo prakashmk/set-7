d3c2106ed399f2bb0e638d99bd6a509a9e96d36a
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-03-02 15:49:28 +0530

    Create p69.c

---
+++ p69.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{ 
+ int a,b,c;
+ printf("Enter the number:");
+ scanf("%d%d",&a,&b);
+ c=a-b;
+  if(c%2==0)
+   {
+     printf("Even number");
+   }
+ else 
+     printf("Odd number");
+     return 0;
+     }
